5a573b3c761450cfa91403423368d561c4789c21
Andreas Katzian <andreas.katzian@jademind.com>
2026-02-26 11:45:59 +0100

    pi-statusbar 0.1.8

--- Formula/pi-statusbar.rb
+++ Formula/pi-statusbar.rb
@@ -1,9 +1,9 @@
 class PiStatusbar < Formula
   desc "Pi macOS status bar app with local daemon and session controls"
   homepage "https://github.com/jademind/pi-statusbar"
-  url "https://github.com/jademind/pi-statusbar/archive/refs/tags/v0.1.7.tar.gz"
-  sha256 "29a8c75968105e7b0aa0b5f72e67c5ad1de6c033c60e727031b189e80fdd0117"
-  version "0.1.7"
+  url "https://github.com/jademind/pi-statusbar/archive/refs/tags/v0.1.8.tar.gz"
+  sha256 "33b91647ee4f7b615ff8b7bd7d9bd9a97b35e80c66007da51cdb5676152a225e"
+  version "0.1.8"
   license "MIT"
   head "https://github.com/jademind/pi-statusbar.git", branch: "main"
 
@@ -14,11 +14,10 @@ class PiStatusbar < Formula
   def install
     libexec.install Dir["*"]
 
-    (bin/"PiStatusBar").write <<~EOS
-      #!/usr/bin/env bash
-      set -euo pipefail
-      exec swift run --package-path "#{opt_libexec}" PiStatusBar "$@"
-    EOS
+    cd libexec do
+      system "swift", "build", "-c", "release", "--product", "PiStatusBar"
+      bin.install ".build/release/PiStatusBar"
+    end
 
     (bin/"statusdctl").write_env_script libexec/"daemon/statusdctl", PI_STATUSBAR_ROOT: libexec
     (bin/"statusd-service").write_env_script libexec/"daemon/statusd-service", PI_STATUSBAR_ROOT: libexec
